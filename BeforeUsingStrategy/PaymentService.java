6fc74696690ab92ea435ead6b4d33b11d8c37589
guanby <66866395+guanby@users.noreply.github.com>
2024-09-22 17:32:27 +0800

    update code

--- BeforeUsingStrategy/PaymentService.java
+++ BeforeUsingStrategy/PaymentService.java
@@ -1,10 +1,21 @@
-public class PaymentStrategy {
+public class PaymentService {
+    private int cost;
+    private boolean includeDeliveryFee;
     
     public void processOrder(String paymentMethod) {
         if ("CreditCard".equals(paymentMethod)) {
             // code to process credit card payment
+        
         } else if ("PayNow".equals(paymentMethod)) {
             // code to process paynow payment
+            
+        } else {
+            // code to process other payment methods
+
         }
+    }
+
+    private int getTotal() {
+        return includeDeliveryFee ? cost + 10 : cost;
     }
 }
